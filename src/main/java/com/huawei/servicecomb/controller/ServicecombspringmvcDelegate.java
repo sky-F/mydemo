404746b8e76802c3c3d88047b71786125ca03054
sky-F <1058207704@qq.com>
2020-11-05 17:15:01 +0800

    Update ServicecombspringmvcDelegate.java

--- src/main/java/com/huawei/servicecomb/controller/ServicecombspringmvcDelegate.java
+++ src/main/java/com/huawei/servicecomb/controller/ServicecombspringmvcDelegate.java
@@ -9,6 +9,6 @@ public class ServicecombspringmvcDelegate {
     public String helloworld(String name){
 
         // Do Some Magic Here!
-        return “Welcome, ” + name;
+        return "Welcome, " + name;
     }
 }
